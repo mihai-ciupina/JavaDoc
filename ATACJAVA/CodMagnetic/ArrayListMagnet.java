268d8575e6397224bad2b65e97b197d60a54702a
mihai-ciupina <mihai.ciupina@gmail.com>
2015-09-22 19:44:06 +0300

    added first application

---
+++ ATACJAVA/CodMagnetic/ArrayListMagnet.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class ArrayListMagnet {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add(0, "zero");
+        a.add(1, "unu");
+        a.add(2, "doi");
+        a.add(3, "trei");
+        printAL(a);
+        
+        a.remove(2);
+        if(a.contains("trei")) {
+            a.add("patru");
+        }
+        printAL(a);
+
+        if(a.indexOf("patru") != 4) {
+            a.add(4, "4.2");
+        }
+        printAL(a);
+        printAL(a);
+
+        
+    }
+
+    public static void printAL(ArrayList<String> al) {
+        for(String element : al) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+    }
+
+    
+}
