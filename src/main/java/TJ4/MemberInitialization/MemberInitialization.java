72b74f408b67b9676902056f3af6b8fe53fe5404
ciupinamihai <ciupinamihai@yahoo.com>
2015-10-11 16:05:36 +0300

    update ...

---
+++ src/main/java/TJ4/MemberInitialization/MemberInitialization.java
@@ -0,0 +1,15 @@
+package TJ4.MemberInitialization;
+
+public class MemberInitialization {
+
+	void f() {
+		//int i;
+		//i++; // Error -- i not initialized
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xxx");
+	}
+
+}
